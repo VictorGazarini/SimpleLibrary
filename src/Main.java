36bb16c3ef0dab3bf82ba6f3163eaad328aa6f9e
VictorGazarini <victorhugogazarini@gmail.com>
2025-05-14 22:50:58 -0300

    testado

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World\nBuild gerada");
-
+        System.out.println("Estudo de array hoje");
         }
     }
